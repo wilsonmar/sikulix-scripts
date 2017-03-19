404026373f156bc381fa2c4da6db220070991512
Wilson Mar <wilsonmar@gmail.com>
2017-03-19 18:55:09 -0400

    mac-spotlight-v02.sikuli update click img

--- mac-spotlight-v02.sikuli/mac-spotlight-v02.py
+++ mac-spotlight-v02.sikuli/mac-spotlight-v02.py
@@ -1,10 +1,11 @@
 # by Wilson Mar
 click("mac-search.png") # Spotlight search on Apple menu
 wait(1)
-click("mac-spotlight-entry.png")
+click("mac-spotlight-entry.png") #mac-spotlight-entry.png
 wait(1)
 # ERROR: Script does not type the beginning of the text:
-click()
+#click()
+#cursor right?
 type("hello world")
 wait(1)
 # exit() # stop here
